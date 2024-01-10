5bf508cc3faf5fc7766e46289d40d734c6e99053
ly <liuyanoutsee@outlook.com>
2024-01-10 19:03:14 +0800

    手写spring系统，实现依赖注入，并解决了循环依赖

--- mini_SpringhasMvc/src/test/java/xmltest/Test1.java
+++ mini_SpringhasMvc/src/test/java/xmltest/Test1.java
@@ -11,21 +11,21 @@ import org.lyflexi.frameworktest.service.SimpleService;
 public class Test1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-        SimpleService simpleService;
-//        AService aService;
-//        BaseService baseService;
-//        BaseBaseService basebaseService;
+//        SimpleService simpleService;
+        AService aService;
+        BaseService baseService;
+        BaseBaseService basebaseService;
         try {
-            simpleService = (SimpleService) ctx.getBean("simpleservice");
-            simpleService.sayHello();
+//            simpleService = (SimpleService) ctx.getBean("simpleservice");
+//            simpleService.sayHello();
 
-//            aService = (AService) ctx.getBean("aservice");
-//            aService.sayHello();
-//            baseService = (BaseService) ctx.getBean("baseservice");
-//            baseService.sayHello();
-//            basebaseService = (BaseBaseService) ctx.getBean("basebaseservice");
-//            basebaseService.sayHello();
-        } catch (BeansException e) {
+            aService = (AService) ctx.getBean("aservice");
+            aService.sayHello();
+            baseService = (BaseService) ctx.getBean("baseservice");
+            baseService.sayHello();
+            basebaseService = (BaseBaseService) ctx.getBean("basebaseservice");
+            basebaseService.sayHello();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
