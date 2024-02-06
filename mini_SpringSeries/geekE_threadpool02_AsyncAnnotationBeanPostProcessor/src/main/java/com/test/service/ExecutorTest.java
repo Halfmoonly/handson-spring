be9c2d72ec7e14ced80ec0011169f71d928cae20
ly <liuyanoutsee@outlook.com>
2024-02-06 20:57:19 +0800

    添加注释：手写mini-spring异步任务带返回值，对比网络编程中异步非阻塞的AIO。总结出异步回调的本质

--- mini_SpringSeries/geekE_threadpool02_AsyncAnnotationBeanPostProcessor/src/main/java/com/test/service/ExecutorTest.java
+++ mini_SpringSeries/geekE_threadpool02_AsyncAnnotationBeanPostProcessor/src/main/java/com/test/service/ExecutorTest.java
@@ -12,6 +12,7 @@ public class ExecutorTest implements ThreadPoolService{
     ThreadPoolTaskExecutor taskExecutor;
 
     public void sayHello() {
+        //这和AIO的异步本质是一样的：AIO用户在进行读写操作时，主线程自己不去等待获取结果（一个线程），而是新起一个子线程（一个线程）向底层操作系统注册回调函数，最后操作系统执行回调函数并向用户程序送结果
         taskExecutor.submitListenable(()->{
             try {
                 Thread.sleep(2000);
@@ -22,7 +23,9 @@ public class ExecutorTest implements ThreadPoolService{
             }
             return 0;
         }).addCallback(data->{System.out.println("sucess "+data);},
-                ex->System.out.println("sucess "+ex));//回调函数onSuccess和onFailure都传递给了Spirng框架，由Spring框架去监听返回结果是否已经创建完成，并主动推送给应用程序（我们）
+                ex->System.out.println("sucess "+ex));//回调函数onSuccess和onFailure都传递给了Spirng框架，由Spring框架去监听返回结果是否已经创建完成，并主动推送给应用程序（我们）。这里的Spring框架就可以类比于操作系统底层
+
+        System.out.println("sayHello's main finish....");
     }
 
 
