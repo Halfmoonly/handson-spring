c3b2db480af978903614fe4f5f9536f240701ec7
ly <liuyanoutsee@outlook.com>
2024-04-21 17:56:05 +0800

    分支5拆分完毕

--- mini_SpringSeries/geekC_jdbc02_expandTemplateAndSingleResponsibilitPrinciple/src/main/java/com/minis/util/ObjectMapper.java
+++
@@ -1,7 +0,0 @@
-package com.minis.util;
-
-public interface ObjectMapper {
-	void setDateFormat(String dateFormat);
-	void setDecimalFormat(String decimalFormat);
-	String writeValuesAsString(Object obj);
-}
