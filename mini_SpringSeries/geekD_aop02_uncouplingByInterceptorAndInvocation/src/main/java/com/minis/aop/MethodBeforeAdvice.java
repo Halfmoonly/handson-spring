eb8b5f90d28866670866646bccdcfb375271aec7
ly <liuyanoutsee@outlook.com>
2024-04-21 19:33:13 +0800

    B-1分支拆分完成

--- mini_SpringSeries/geekD_aop02_uncouplingByInterceptorAndInvocation/src/main/java/com/minis/aop/MethodBeforeAdvice.java
+++
@@ -1,7 +0,0 @@
-package com.minis.aop;
-
-import java.lang.reflect.Method;
-
-public interface MethodBeforeAdvice extends BeforeAdvice{
-	void before(Method method, Object[] args, Object target) throws Throwable;
-}
--- mini_SpringSeries/geekD_aop04_AutoProxyCreatorToMatchclasss/src/main/java/com/minis/aop/PointcutAdvisor.java
+++
@@ -1,5 +0,0 @@
-package com.minis.aop;
-
-public interface PointcutAdvisor extends Advisor {
-	Pointcut getPointcut();
-}
