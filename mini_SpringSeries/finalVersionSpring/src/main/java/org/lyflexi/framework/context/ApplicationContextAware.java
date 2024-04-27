9c47d8226fb3f1cf0abf50ea1feff37b82159c68
ly <liuyanoutsee@outlook.com>
2024-04-27 12:21:34 +0800

    geekC_jdbc03_PooledDataSource分支拆分完成

--- mini_SpringSeries/finalVersionSpring/src/main/java/org/lyflexi/framework/context/ApplicationContextAware.java
+++
@@ -1,7 +0,0 @@
-package org.lyflexi.framework.context;
-
-import org.lyflexi.framework.beans.BeansException;
-
-public interface ApplicationContextAware {
-	void setApplicationContext(ApplicationContext applicationContext) throws BeansException;
-}
--- mini_SpringSeries/geekB_mvc01_xmlMapping/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World tomcat9.0.6 mvc01 start!</h2>
-</body>
-</html>
