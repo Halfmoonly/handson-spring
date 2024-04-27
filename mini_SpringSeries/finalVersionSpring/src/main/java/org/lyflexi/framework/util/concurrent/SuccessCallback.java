3980236df4c5cc6c2fda1c0349f7940859b0a9bd
ly <liuyanoutsee@outlook.com>
2024-04-27 11:38:45 +0800

    geekB_mvc06_requestDataBinder分支拆分完成

--- mini_SpringSeries/finalVersionSpring/src/main/java/org/lyflexi/framework/util/concurrent/SuccessCallback.java
+++
@@ -1,5 +0,0 @@
-package org.lyflexi.framework.util.concurrent;
-
-public interface SuccessCallback<T> {
-	void onSuccess(T result);
-}
--- mini_SpringSeries/geekE_threadpool01_addCallback/src/main/java/com/minis/aop/framework/AopProxyFactory.java
+++
@@ -1,7 +0,0 @@
-package com.minis.aop.framework;
-
-import com.minis.aop.Advisor;
-
-public interface AopProxyFactory {
-	AopProxy createAopProxy(Object target, Advisor adviseor);
-}
