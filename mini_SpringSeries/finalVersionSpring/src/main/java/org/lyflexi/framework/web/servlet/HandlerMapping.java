6a054e59f042ab50c4cb6466750d37d50f313043
ly <liuyanoutsee@outlook.com>
2024-04-27 12:03:54 +0800

    geekC_jdbc01_IntegratediocJdbcTemplateAndDataSource分支拆分完成

--- mini_SpringSeries/finalVersionSpring/src/main/java/org/lyflexi/framework/web/servlet/HandlerMapping.java
+++
@@ -1,9 +0,0 @@
-package org.lyflexi.framework.web.servlet;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.lyflexi.framework.web.method.HandlerMethod;
-
-public interface HandlerMapping {
-	HandlerMethod getHandler(HttpServletRequest request) throws Exception;
-}
