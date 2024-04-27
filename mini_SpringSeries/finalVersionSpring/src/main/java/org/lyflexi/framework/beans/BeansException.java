cf9a9fe77744d33896112841b32ceaf1e6665e2d
ly <liuyanoutsee@outlook.com>
2024-04-27 11:31:30 +0800

    geekB_mvc05_HanlderMappingAndAdapter分支拆分完成

--- mini_SpringSeries/finalVersionSpring/src/main/java/org/lyflexi/framework/beans/BeansException.java
+++
@@ -1,7 +0,0 @@
-package org.lyflexi.framework.beans;
-
-public class BeansException extends Exception {
-	public BeansException(String msg) {
-		super(msg);
-	}
-}
--- mini_SpringSeries/finalVersionSpring/src/main/java/org/lyflexi/framework/web/bind/support/WebBindingInitializer.java
+++
@@ -1,7 +0,0 @@
-package org.lyflexi.framework.web.bind.support;
-
-import org.lyflexi.framework.web.bind.WebDataBinder;
-
-public interface WebBindingInitializer {
-	void initBinder(WebDataBinder binder);
-}
